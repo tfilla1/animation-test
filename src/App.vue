cc5a6e2654ff45a30da0012cb7084e7c1fdd9821
tfilla <tfilla.primary@gmail.com>
2022-07-28 18:24:13 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="start">start</button>
+    <button @click="pause">pause</button>
+    <div class="wrapper">
+      <div class="item">
+        <span class="letter">hello</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import anime from 'animejs';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    animation: null,
+    letter: null
+  }),
+  methods: {
+    start() {
+      this.animation.play();
+    },
+    pause() {
+      this.animation.pause();
+    }
+  },
+  mounted() {
+    this.animation = anime({
+      targets: '.item',
+      backgroundColor: ['#f0a', '#0af', '#a0f', '#0af', '#f0a'],
+      translateY: [0, 250, 450, 250, 0],
+      translateX: [0, 250, 450, 250, 0],
+      rotate: [0, 50, 0, 75, 0],
+      duration: 3500,
+      scale: [1, 2, 1, 2, 1],
+      text: ['', 'hi', ''],
+      loop: true
+    });
+
+    this.letter = anime({
+      targets: '.letter',
+      backgroundColor: ['#fff', '#000'],
+      translateY: 50
+    })
   }
 }
 </script>
@@ -24,5 +56,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrapper {
+  background-color: #000;
+  width: 500px;
+  height: 500px;
+}
+.item {
+  background-color: #ff00aa;
+  width: 50px;
+  height: 50px;
 }
 </style>
