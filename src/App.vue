9feca04e0ecb36d67e4305519f3dc3e351b0e400
Tom Filla <tfilla1@gmail.com>
2022-07-31 07:10:03 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
+    <h1>
+      <span class="letter">h</span>
+      <span class="letter">e</span>
+      <span class="letter">l</span>
+      <span class="letter">l</span>
+      <span class="letter">o</span>
+    </h1>
     <button @click="start">start</button>
     <button @click="pause">pause</button>
-    <div class="wrapper">
-      <div class="item">
-        <span class="letter">hello</span>
-      </div>
+    <div class="wrapper d-none">
+      <div class="item"></div>
     </div>
+    <br />
   </div>
 </template>
 
 <script>
-import anime from 'animejs';
+import anime from "animejs";
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     animation: null,
-    letter: null
+    timeline: null,
   }),
   methods: {
     start() {
@@ -24,30 +30,69 @@ export default {
     },
     pause() {
       this.animation.pause();
-    }
+    },
   },
   mounted() {
+    // this.animation = anime({
+    //   targets: ".letter",
+    //   backgroundColor: ["#f0a", "#af0", "#f0a"],
+    //   scale: [1, 2, 1],
+    // });
     this.animation = anime({
-      targets: '.item',
-      backgroundColor: ['#f0a', '#0af', '#a0f', '#0af', '#f0a'],
-      translateY: [0, 250, 450, 250, 0],
-      translateX: [0, 250, 450, 250, 0],
-      rotate: [0, 50, 0, 75, 0],
-      duration: 3500,
-      scale: [1, 2, 1, 2, 1],
-      text: ['', 'hi', ''],
-      loop: true
+      targets: ".letter",
+      top: 0,
+      left: 0,
+      opacity: 1,
+      translateX: [0, 50],
+      translateY: [90, 180],
+      rotate: {
+        value: 720,
+        duration: 2000,
+        easing: "easeOutExpo",
+      },
+      scale: anime.stagger([1, 2, 1, 2, 1, 2], { from: "center" }),
+      delay: anime.stagger(1000, { start: 0 }),
+      loop: true,
     });
 
-    this.letter = anime({
-      targets: '.letter',
-      
-    })
-  }
-}
+    this.timeline = anime.timeline({
+      easing: "easeOutExpo",
+      duration: 750,
+      loop: true,
+    });
+
+    this.timeline
+      .add({
+        targets: ".item",
+        backgroundColor: ["#f0a", "#0af", "#a0f", "#0af", "#f0a"],
+        translateY: [0, 250, 450, 250, 0],
+        translateX: [0, 250, 450, 250, 0],
+        rotate: [0, 180, 0, 180, 0],
+        duration: 3500,
+        scale: [1, 4, 1, 4, 1],
+        loop: true,
+      })
+      .add({
+        targets: ".item",
+        translateX: [0, 250, 0],
+        backgroundColor: ["#f0a", "#a0f", "#f0a"],
+        duration: 1500,
+      });
+  },
+};
 </script>
 
 <style>
+.letter {
+  display: inline-block;
+  opacity: 0;
+  top: 0;
+  left: 0
+
+}
+.d-none {
+  display: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
